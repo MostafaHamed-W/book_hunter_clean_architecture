365dd761f4eca3008ef45eafc1700d528d8d35c0
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2023-08-16 17:20:47 +0300

    handle failures and exceptions

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:book_hunt/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
